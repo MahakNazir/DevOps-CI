28e78c1448636afdd8399602c84ed9c290920e84
Mahak Nazir <122639932+MahakNazir@users.noreply.github.com>
2024-10-01 00:21:34 +0530

    Update GreetMahak.java

--- GreetMahak.java
+++ GreetMahak.java
@@ -2,5 +2,6 @@ public class GreetMahak {
     public static void main(String[] args) {
         System.out.println("Hi Mahak, this side");
          System.out.println("I'm trying CI on Jnekins");
+        System.out.println("It's working, wuhooo!");
     }
 }
